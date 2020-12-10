e1a89aa5a095208efd0e2a0d2eb8563e75bf8e61
Daniil Demichev <daniil_demichev@mail.ru>
2020-12-10 09:54:01 +0300

    Initial commit

---
+++ src/main/java/com/xxpffxx/bd_back/BdBackApplication.java
@@ -0,0 +1,13 @@
+package com.xxpffxx.bd_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BdBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xxpffxx/bd_back/BdBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxpffxx.bd_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
